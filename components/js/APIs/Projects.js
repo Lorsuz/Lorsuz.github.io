258f03334dcd16d76c55c70f460e946e06d46a9f
Lorsuz <91427731+Lorsuz@users.noreply.github.com>
2023-10-18 20:24:23 -0300

    ddfd

--- components/js/APIs/Projects.js
+++ components/js/APIs/Projects.js
@@ -6,23 +6,32 @@ fetch( "data.json" ).then( ( response ) => {
 			Projects.innerHTML +=
 				`
 				<li class="card">
-					<div class="img"><img src="components/static/img/projects/${card.image}" alt="${card.image}">
+					<div class="img"><img src="components/static/img/projects/${ card.image }" alt="${ card.image }">
 					</div>
 					<div class="animation">
 						<div class="details">
-						<h3>${card.project}</h3>
-							<div class="skill-tags">${
-								card.skills.map( ( skill ) => {
-									return `<span class="tag">${skill}</span>`;
-								} ).join( "" )
-							}
+						<h3>${ card.project }</h3>
+							<div class="skill-tags">${ card.skills.map( ( skill ) => {
+					return `<span class="tag">${ skill }</span>`;
+				} ).join( "" )
+				}
 							</div>
-							<p>${card.description}</p>
+							<p>${ card.description }</p>
 							<div class="links">
-								<a href="https://github.com/Lorsuz/${card.repository}" target="_blank">Repository</a>
-								${
-									card.pages ?	`<a href="${card.pages}" target="_blank">Web Site</a>`:""
-								}
+							${ card.repository ?
+								`<a href="https://github.com/Lorsuz/${ card.repository }" target="_blank">Repository</a>` :
+								""
+							}
+							
+							${ card.pages ?
+								`<a href="${ card.pages }" target="_blank">Web Site</a>` :
+								""
+							}
+
+							${ !card.pages && !card.repository ?
+								`<span>There're no links available for this project yet!!</span>` :
+								""
+							}
 							</div>
 						</div>
 					</div>
--- components/js/classes/Slide.js
+++ components/js/classes/Slide.js
@@ -1,31 +1,29 @@
 export default class Slide {
-	slide;
+	slides;
 	prev;
 	next;
-	dot;
+	dots;
 	slideCurrent = 0;
 	totalSlides;
 	interval;
 
-	constructor ( slide, prev, next, dot ) {
-		this.slide = document.querySelectorAll( slide );
+	constructor ( slides, prev, next, dots ) {
+		this.slides = document.querySelectorAll( slides );
 		this.prev = document.querySelector( prev ).addEventListener( "click", () => {
 			this.calculateSlideCurrent( -1 );
 		} );
 		this.next = document.querySelector( next ).addEventListener( "click", () => {
 			this.calculateSlideCurrent( 1 );
 		} );
-		this.dot = document.querySelectorAll( dot );
-		for ( let index = 0; index < this.dot.length; index++ ) {
-			this.dot[ index ].addEventListener( "click", () => {
+		this.dots = document.querySelectorAll( dots );
+		for ( let index = 0; index < this.dots.length; index++ ) {
+			this.dots[ index ].addEventListener( "click", () => {
 				this.removeAnimation();
 				this.animationEffect( this.slideCurrent, index );
 				this.changeSlideCurrent( index );
 			} );
 		}
-		this.totalSlides = this.slide.length - 1;
-		this.slide[ 0 ].classList.add( "active" );
-
+		this.totalSlides = this.slides.length - 1;
 		this.init();
 	}
 
@@ -51,34 +49,34 @@ export default class Slide {
 
 	animationEffect ( slideCurrent, index, value = 0 ) {
 		if ( value == 1 ) {
-			this.slide[ slideCurrent ].classList.add( "toRightOld" );
-			this.slide[ index ].classList.add( "toRightNew" );
+			this.slides[ slideCurrent ].classList.add( "toRightOld" );
+			this.slides[ index ].classList.add( "toRightNew" );
 		} else if ( value == -1 ) {
-			this.slide[ this.slideCurrent ].classList.add( "toLeftOld" );
-			this.slide[ index ].classList.add( "toLeftNew" );
+			this.slides[ this.slideCurrent ].classList.add( "toLeftOld" );
+			this.slides[ index ].classList.add( "toLeftNew" );
 		} else if ( slideCurrent < index ) {
-			this.slide[ slideCurrent ].classList.add( "toRightOld" );
-			this.slide[ index ].classList.add( "toRightNew" );
+			this.slides[ slideCurrent ].classList.add( "toRightOld" );
+			this.slides[ index ].classList.add( "toRightNew" );
 		} else if ( slideCurrent > index ) {
-			this.slide[ slideCurrent ].classList.add( "toLeftOld" );
-			this.slide[ index ].classList.add( "toLeftNew" );
+			this.slides[ slideCurrent ].classList.add( "toLeftOld" );
+			this.slides[ index ].classList.add( "toLeftNew" );
 		}
 
 	}
 
 	changeSlideCurrent ( index ) {
-		this.slide[ this.slideCurrent ].classList.remove( "active" );
-		this.dot[ this.slideCurrent ].classList.remove( "active" );
+		this.slides[ this.slideCurrent ].classList.remove( "active" );
+		this.dots[ this.slideCurrent ].classList.remove( "active" );
 		this.slideCurrent = index;
-		this.slide[ this.slideCurrent ].classList.add( "active" );
-		this.dot[ this.slideCurrent ].classList.add( "active" );
+		this.slides[ this.slideCurrent ].classList.add( "active" );
+		this.dots[ this.slideCurrent ].classList.add( "active" );
 
 		this.init();
 	}
 
 	removeAnimation () {
 		clearInterval( this.interval );
-		this.slide.forEach( element => {
+		this.slides.forEach( element => {
 			element.classList.remove( "toRightOld" );
 			element.classList.remove( "toRightNew" );
 			element.classList.remove( "toLeftOld" );
